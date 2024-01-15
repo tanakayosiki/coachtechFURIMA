808109f9ce7b241975e153acadcf27e70bd8f46a
tanaka yoshiki <a10hhmotbssl@icloud.com>
2024-01-16 04:06:32 +0900

    ログイン機能追加

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ItemController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +14,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/login',[AuthController::class,'getLogin']);
+Route::post('/login',[AuthController::class,'postLogin']);
+Route::get('/register', [AuthController::class,'getRegister']);
+Route::post('/register', [AuthController::class,'postRegister']);
+Route::get('/',[ItemController::class,'index']);

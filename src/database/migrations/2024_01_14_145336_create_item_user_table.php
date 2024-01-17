1cb542a3ec01b5ce199db9de526b1361b7ac1793
tanaka yoshiki <a10hhmotbssl@icloud.com>
2024-01-18 05:43:32 +0900

    テーブル名変更

--- src/database/migrations/2024_01_14_145336_create_item_user_table.php
+++ src/database/migrations/2024_01_14_145336_create_item_user_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSellsTable extends Migration
+class CreateItemUserTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateSellsTable extends Migration
      */
     public function up()
     {
-        Schema::create('sells', function (Blueprint $table) {
+        Schema::create('item_user', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->foreignId('item_id')->constrained()->cascadeOnDelete();
@@ -28,6 +28,6 @@ class CreateSellsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('sells');
+        Schema::dropIfExists('item_user');
     }
 }
